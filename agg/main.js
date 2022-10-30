a2a51d4cfa93df4c7a38213a4fa272e2de5a6e52
Thomas Manning <thomasm@balena.io>
2022-10-30 22:32:40 +1000

    Pass matrix output to agg

--- agg/main.js
+++ agg/main.js
@@ -1,3 +1,10 @@
 const core = require('@actions/core');
+
+const out_1 = core.getInput('out_1')
+const out_2 = core.getInput('out_2')
+const out_3 = core.getInput('out_3')
+
+console.log('Received op outputs', out_1 || 'null', out_2 || 'null', out_3 || 'null')
+
 const ops = [{ id: 1 },{ id: 2 },{ id: 3 }]
 core.setOutput('ops', JSON.stringify(ops))
--- op/main.js
+++ op/main.js
@@ -3,3 +3,5 @@ const core = require('@actions/core');
 const id = core.getInput('id')
 
 console.log(JSON.stringify(id))
+
+core.setOutput(`out_${id}`, id)
